559d26f47639cc54c66bb8f52557a9fe707d777b
Ruestap <ruesta.r@corporacionvega.pe>
2026-03-21 18:08:48 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyANFjMRfZ9mwXpBm637H4E3AaYGf_MCtI8",
   authDomain: "vega-desing-tracker.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:176230006788:web:58de52ed6e1c56fc037551"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
